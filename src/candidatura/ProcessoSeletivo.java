5459e46935dcde1b7605619c762e45a3b21f0a31
rafammassa <rafaella_massa@hotmail.com>
2024-06-12 14:24:12 -0300

    Adicionando método selecionarCandidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,13 +1,32 @@
 package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
-        analisarCandidato(1900);
-        analisarCandidato(2200);
-        analisarCandidato(2000);
+        selecionarCandidatos();
     }
 
+    static void selecionarCandidatos(){
+        String [] candidatos = {"FELIPE", "MARCIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JOREL", "MARCELA"};
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato " + candidato + " solicitou o salário de R$" + salarioPretendido);
+            if (salarioBase >= salarioPretendido){
+                candidatosSelecionados++;
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga!");
+            }
+            candidatoAtual++;
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1000,2200);
+    }
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000;
         if (salarioBase > salarioPretendido){
